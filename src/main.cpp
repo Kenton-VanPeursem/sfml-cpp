80ff9e906740b5999252bf0e0a15ee2e5e97c8db
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2023-07-16 14:09:34 -1000

    Rotate tile with 'r' key

--- src/main.cpp
+++ src/main.cpp
@@ -20,21 +20,34 @@
 #define SELECTION_PADDING 6
 #define SELECTION_ALPHA 0.75
 
-int main() {
+void resetOrigin(sf::Sprite *sprite, int rotation) {
+  switch (rotation) {
+  case 0:
+    sprite->setOrigin(sf::Vector2f(0.f, 0.f));
+    break;
+  case 1:
+    sprite->setOrigin(sf::Vector2f(0.f, TILE_SIZE));
+    break;
+  case 2:
+    sprite->setOrigin(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+    break;
+  case 3:
+    sprite->setOrigin(sf::Vector2f(TILE_SIZE, 0.f));
+    break;
+  default:
+    break;
+  }
+}
 
+int main() {
   sf::Texture texture;
   if (!texture.loadFromFile(TILE_MAP)) {
     std::cout << "Failed to load texture from " << TILE_MAP << std::endl;
     return EXIT_FAILURE;
   }
 
-  sf::ContextSettings contextSettings;
-  contextSettings.antialiasingLevel = 8;
-
   sf::Vector2u mapSize = sf::Vector2u(TILE_SIZE * WINDOW_WIDTH_TILES,
                                       TILE_SIZE * WINDOW_HEIGHT_TILES);
-  // set the window height to be the max of the mapSize height and the TILE_SIZE
-  // * WINDOW_HEIGHT_TILES
   size_t width, height;
   if (mapSize.y < texture.getSize().y) {
     height = texture.getSize().y;
@@ -43,9 +56,14 @@ int main() {
   }
   width = TILE_SIZE * WINDOW_WIDTH_TILES + texture.getSize().x;
 
+
+  // Window setup -------------------------------------------------------------
+  sf::ContextSettings contextSettings;
+  contextSettings.antialiasingLevel = 8;
   sf::RenderWindow window(sf::VideoMode(width, height), "My Window",
                           sf::Style::Default, contextSettings);
   window.setFramerateLimit(60);
+  //---------------------------------------------------------------------------
 
   sf::Sprite selectionArea(texture);
   // center the selection area vertically
@@ -62,6 +80,7 @@ int main() {
 
   sf::Sprite pointer = sf::Sprite(tileFactory.getTile(0));
   pointer.setColor(sf::Color(255, 255, 255, SELECTION_ALPHA * 255));
+  int rotation = 0;
 
   std::vector<sf::Sprite> placedTiles;
   sf::Color clearColor = sf::Color::White;
@@ -77,7 +96,10 @@ int main() {
 
       if (event.type == sf::Event::Closed) {
         window.close();
-      } else if (event.type == sf::Event::MouseButtonPressed) {
+      }
+
+      // handle mouse clicks
+      if (event.type == sf::Event::MouseButtonPressed) {
         if (event.mouseButton.button == sf::Mouse::Left) {
           // check if mouse is in the selectable area
           sf::Vector2f mousePosition(event.mouseButton.x, event.mouseButton.y);
@@ -88,6 +110,8 @@ int main() {
             int tileX = int(mousePosition.x) / TILE_SIZE;
             int tileY = int(mousePosition.y - selectionAreaPadding) / TILE_SIZE;
             pointer = tileFactory.getTile(tileX, tileY);
+            pointer.setColor(sf::Color(255, 255, 255, SELECTION_ALPHA * 255));
+            rotation = 0; // reset rotation
           } else if (!selectionBackground.getGlobalBounds().contains(
                          mousePosition)) {
             sf::Sprite csprite(pointer);
@@ -112,11 +136,27 @@ int main() {
             }
           }
         }
-      } else if (event.type == sf::Event::MouseMoved) {
+      }
+
+      // pointer updates with mouse movement
+      if (event.type == sf::Event::MouseMoved) {
         // get the nearest position to put the sprite on a grid
         int x = event.mouseMove.x - (event.mouseMove.x % TILE_SIZE);
         int y = event.mouseMove.y - (event.mouseMove.y % TILE_SIZE);
-        pointer.setPosition(sf::Vector2f(x, y));
+        sf::Vector2f pos(x, y);
+        pointer.setRotation(rotation * 90);
+        resetOrigin(&pointer, rotation);
+        pointer.setPosition(pos);
+      }
+
+      if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::R) {
+          rotation = (rotation + 1) % 4;
+          sf::Vector2f pos(pointer.getPosition());
+          pointer.setRotation(rotation * 90);
+          resetOrigin(&pointer, rotation);
+          pointer.setPosition(pos);
+        }
       }
     }
 
